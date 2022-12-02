f304d51c1a8cbecfea37652ce81e94595775d36a
Aleksandr Kornilov <saxa03@mail.ru>
2022-12-02 22:27:34 +0800

    add Task 004 HomeWork

---
+++ Task_004_HomeWork/Program.cs
@@ -0,0 +1,16 @@
+﻿//вывести какое число большее а какое меньшее
+Console.Clear();
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+
+if ( a > b)
+{
+    Console.WriteLine($"{a}");
+}
+else
+{
+   Console.WriteLine($"{b}"); 
+}
+
