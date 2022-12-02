efd06372c71e775eec9cc7d40a99648a8d5a365a
Aleksandr Kornilov <saxa03@mail.ru>
2022-12-02 19:18:35 +0800

    add Task 003 WeekDay

---
+++ Task_003/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Clear();
+Console.Write("Введите день недели от 1 до 7 ");
+string s = Console.ReadLine();
+switch (s)
+{
+    case "1":
+    Console.WriteLine("Понедельник");
+    break;
+    case "2":
+    Console.WriteLine("Вторник");
+    break;
+    case "3":
+    Console.WriteLine("Среда");
+    break;
+    case "4":
+    Console.WriteLine("Четверг");
+    break;
+    case "5":
+    Console.WriteLine("Пятница");
+    break;
+    case "6":
+    Console.WriteLine("Суббота");
+    break;
+    case "7":
+    Console.WriteLine("Воскресенье");
+    break;
+}
