1da4abecc7ae7434f13954728dc9118a872731f8
Aleksandr Kornilov <saxa03@mail.ru>
2022-12-02 22:46:13 +0800

    add Task 007 HomeWork

---
+++ Task_007_HomeWork/Program.cs
@@ -0,0 +1,13 @@
+﻿// является ли число четным
+Console.Clear();
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+
+if ( a%2 == 0 )
+{
+    Console.WriteLine(" четное ");
+} 
+else 
+{
+    Console.WriteLine(" нечетное ");
+} 
