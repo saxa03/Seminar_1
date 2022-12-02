c4fb27558920ab088aee2af1c6a87feaf0cd8f85
Aleksandr Kornilov <saxa03@mail.ru>
2022-12-02 22:05:12 +0800

    add Task 005 HomeWork

---
+++ Task_005_HomeWork/Program.cs
@@ -0,0 +1,16 @@
+﻿//вывести максимальное из трех чисел
+Console.Clear();
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int c = int.Parse(Console.ReadLine());
+int max = a;
+
+if ( a > max ) max = a;
+if ( b > max ) max = b;
+if ( c > max ) max = c;
+
+Console.Write("max = ");
+Console.WriteLine(max);
