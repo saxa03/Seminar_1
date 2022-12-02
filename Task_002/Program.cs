eea78e1a0594ee10113acedafe74b2d099df7a18
Aleksandr Kornilov <saxa03@mail.ru>
2022-12-02 18:40:43 +0800

    add Task 002

---
+++ Task_002/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("a: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("b: ");
+int b = int.Parse(Console.ReadLine());
+if(Math.Pow(b,2) == a)
+{
+    Console.WriteLine("a квадрат b");
+}
+else
+{
+   Console.WriteLine("a не квадрат b"); 
+}
