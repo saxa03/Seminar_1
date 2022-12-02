16665b7ece8e77bf8dd7f2008109acab4e0fa3f9
Aleksandr Kornilov <saxa03@mail.ru>
2022-12-02 23:21:20 +0800

    add Task 009 Homework

---
+++ Task_009_HomeWork/Program.cs
@@ -0,0 +1,15 @@
+﻿// вывести все четные числа от 1 до N
+Console.Clear();
+Console.Write("Введите число: ");
+
+int N = int.Parse(Console.ReadLine());
+int B = 2;
+
+while ( B <= N )
+
+{
+    if ( B%2 == 0)
+    
+    Console.WriteLine($"{B}");
+    B++;
+}
